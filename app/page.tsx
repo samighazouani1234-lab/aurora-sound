78b64c2f90e5945248343b49735a98c868e248a6
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-04-26 15:41:00 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{color: "white", background: "black", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <h1>Aurora Sound 🚀</h1>
+    </div>
+  );
+}
